d9aedc45d7148f443654c1743e2f9dfa88a1deab
momen5406 <morenocoder@gmail.com>
2025-10-04 11:19:17 +0300

    Final

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,27 @@
-import React, { forwardRef } from 'react'
-import { twMerge } from 'tailwind-merge'
+import React, { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        type={type}
+        className={twMerge(
+          `w-full rounded-full bg-green-500 border-transparent p-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`,
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
-  className, children, disabled, type= 'button', ...props
-}, ref) => {
+Button.displayName = "Button";
 
-  return (
-    <button type={type} className={twMerge(`w-full rounded-full bg-green-500 border-transparent p-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`, className)} disabled={disabled} ref={ref} {...props}>
-      {children}
-    </button>
-  )
-})
-
-Button.displayName = 'Button'
-
-export default Button
+export default Button;
