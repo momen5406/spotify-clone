4eece7a1cd1422afe1438a69de83f32d9e634725
momen5406 <morenocoder@gmail.com>
2025-10-04 11:25:59 +0300

    Final

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,23 +1,24 @@
-"use client"
-import { Database } from "@/types_db"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { SessionContextProvider } from "@supabase/auth-helpers-react"
-import { useState } from "react"
+"use client";
+import { Database } from "@/types_db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 interface SupabaseProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const SupabaseProvider: React.FC<SupabaseProviderProps> = ({children}) => {
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
   const [supabaseClient] = useState(() =>
     createClientComponentClient<Database>()
   );
 
   return (
-    <SessionContextProvider supabaseClient={supabaseClient}>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    <SessionContextProvider supabaseClient={supabaseClient as any}>
       {children}
     </SessionContextProvider>
-  )
-}
+  );
+};
 
-export default SupabaseProvider
+export default SupabaseProvider;
